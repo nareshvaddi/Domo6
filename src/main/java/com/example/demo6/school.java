87aa4111e1284e1827cbecb9816110598f001c6e
nareshvaddi <nareshvaddi88@gmail.com>
2021-11-02 21:24:59 +0530

    newfileadding

---
+++ src/main/java/com/example/demo6/school.java
@@ -0,0 +1,21 @@
+package com.example.demo6;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@Component
+public class school {
+	
+	
+	private int id;
+	private String fname;
+	private String Lname;
+	
+
+}
